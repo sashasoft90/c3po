7f9ea9601fb07bc8738cdb377223a749796ef562
Alex Sacharov <alexander.sacharov@t-online.de>
2025-10-24 23:12:28 +0200

    C3PO-1/AlSa: add avatar components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button/index.js";
+	import * as Avatar from "$lib/components/ui/avatar/index.js";
 </script>
 
 
@@ -8,3 +9,8 @@
 
 
 <Button>Click me</Button>
+
+<Avatar.Root>
+	<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+	<Avatar.Fallback>CN</Avatar.Fallback>
+</Avatar.Root>
