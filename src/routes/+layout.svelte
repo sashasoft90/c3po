b908735d61c1c1d595e65e55a37eb870e3d680fd
Alex Sacharov <alexander.sacharov@t-online.de>
2025-10-26 22:05:25 +0100

    C3PO-1/AlSa: add watcher mode and toggle button.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { ModeWatcher } from "mode-watcher";
+	import ModeWatcherToggle from '@/ui/mode_watcher_toggle/mode_watcher_toggle.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+<ModeWatcher />
+<ModeWatcherToggle/>
 {@render children?.()}
+
+<div class="fixed bottom-0 right-0 p-4 accent-red-800">asd</div>
