fc1a5f4dcf168f4f9ba2385630ac6b8780234780
Alex Sacharov <alexander.sacharov@t-online.de>
2025-10-28 20:54:04 +0100

    C3PO-1/AlSa: fix body-wrapper

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,11 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<NavBar navBarItems={navBarItems}></NavBar>
 
-<div id="body-wrapper" class="flex m-2 p-2 flex-col min-h-screen">
-	{@render children?.()}
+
+<div id="body-wrapper" class="flex flex-col min-h-screen">
+	<NavBar navBarItems={navBarItems}></NavBar>
+	<div class="p-2 m-2">
+		{@render children?.()}
+	</div>
 </div>
