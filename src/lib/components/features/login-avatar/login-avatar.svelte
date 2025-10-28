1d4458073ba41304abb7257b76aef80133614c3f
Alex Sacharov <alexander.sacharov@t-online.de>
2025-10-28 20:48:32 +0100

    C3PO-1/AlSa: add avatar

---
+++ src/lib/components/features/login-avatar/login-avatar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as Avatar from '@/ui/avatar'
+
+	let {...restProps} = $props();
+</script>
+
+<Avatar.Root {...restProps}>
+	<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+	<Avatar.Fallback>CN</Avatar.Fallback>
+</Avatar.Root>
