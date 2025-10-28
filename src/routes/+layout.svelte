16b6bbca36196c8eb5ae034f4bb7f0f1c206350e
Alex Sacharov <alexander.sacharov@t-online.de>
2025-10-28 20:50:41 +0100

    C3PO-1/AlSa: add new routes for main pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from "mode-watcher";
-	import ModeWatcherToggle from '@/ui/mode_watcher_toggle/mode_watcher_toggle.svelte';
+	import NavBar from '@/widgets/nav-bar/nav-bar.svelte';
+	import navBarItems from '$lib/pages/store/routes';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-<ModeWatcher />
-<ModeWatcherToggle/>
-{@render children?.()}
 
-<div class="fixed bottom-0 right-0 p-4 accent-red-800">asd</div>
+<NavBar navBarItems={navBarItems}></NavBar>
+
+<div id="body-wrapper" class="flex m-2 p-2 flex-col min-h-screen">
+	{@render children?.()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
-	import * as Avatar from "$lib/components/ui/avatar/index.js";
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-
-<Button>Click me</Button>
-
-<Avatar.Root>
-	<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-	<Avatar.Fallback>CN</Avatar.Fallback>
-</Avatar.Root>
+<h3 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl ">Welcome C3PO</h3>
